f1a0b435fb5cb55936be03385515b07d623088db
niraj.patil <niraj.patil@pwc.com>
2021-09-14 16:58:30 +0530

    add data validation rules in usser model

--- viewtube-backend-/AuthServer/Models/User.cs
+++ viewtube-backend-/AuthServer/Models/User.cs
@@ -9,12 +9,22 @@ namespace AuthServer
         [JsonProperty(PropertyName = "userId")]
         public int UserId { get; set; }
 
+        [Required(ErrorMessage = "Employee Name is required")]  
+        [StringLength(35, MinimumLength = 3)]
         [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Please enter your email address")]  
+        [DataType(DataType.EmailAddress)]  
+        [EmailAddress]
+        [Display(Name = "Email address")]  
+        [MaxLength(50)]  
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Please enter correct email")]
         [JsonProperty(PropertyName = "email")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Password is required")]  
+        [StringLength(18, MinimumLength = 8)]
         [JsonProperty(PropertyName = "password")]
         public string Password { get; set; }
 
